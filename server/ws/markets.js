0c245ab65cd66f313153541ef5c0ae0338a22cf2
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-04-28 13:39:25 +0800

    perf: improve broadcast algo

--- server/ws/markets.js
+++ server/ws/markets.js
@@ -3,30 +3,31 @@ const WebSocket = require('ws')
 const { WebSocketServer } = require('ws')
 
 const arbitrage = require('../arbitrage')
-const wait = require('../utils/wait')
 
 const wss = new WebSocketServer({ noServer: true })
 
-wss.on('connection', async (ws) => {
+wss.on('connection', (ws) => {
   ws.on('error', console.error)
-
-  while (wss.clients.size > 0) {
-    const pair = arbitrage.getPair()
-    const data = JSON.stringify(pair.map((ex) => ({
-      name: ex.name,
-      ask: ex.ask.toNumber(),
-      bid: ex.bid.toNumber()
-    })))
-
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data)
-      }
-    })
-
-    await wait(1000)
-  }
 })
+
+async function broadcastMarket () {
+  const pair = arbitrage.getPair()
+  const timestamp = Date.now()
+  const data = JSON.stringify(pair.map((ex) => ({
+    name: ex.name,
+    ask: ex.ask.toNumber(),
+    bid: ex.bid.toNumber(),
+    timestamp
+  })))
+  
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data)
+    }
+  })
+}
+
+setInterval(broadcastMarket, 1000)
 
 module.exports = {
   name: 'markets',
