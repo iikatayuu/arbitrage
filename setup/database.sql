9e562d58e94a78a667d12f4307cd33b88002799c
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-05-28 13:13:46 +0800

    perf: update table structure

--- setup/database.sql
+++ setup/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 27, 2023 at 03:10 PM
--- Server version: 8.0.27
--- PHP Version: 7.4.26
+-- Generation Time: May 28, 2023 at 05:12 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,16 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `config`
+--
+
+DROP TABLE IF EXISTS `config`;
+CREATE TABLE IF NOT EXISTS `config` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Config ID',
+  `name` varchar(255) NOT NULL COMMENT 'Config Key',
+  `value` text NOT NULL COMMENT 'Config Value',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `config`
+--
+
+INSERT INTO `config` (`id`, `name`, `value`) VALUES
+(1, 'symbol', '0');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `trades`
 --
 
 DROP TABLE IF EXISTS `trades`;
 CREATE TABLE IF NOT EXISTS `trades` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'Trade ID',
-  `buy_usd` float(16,2) NOT NULL COMMENT 'Buy value (USD)',
-  `sell_btc` float(16,8) NOT NULL COMMENT 'Sell value (BTC)',
+  `buy_ex` varchar(255) NOT NULL COMMENT 'Exchange bought from',
+  `sell_ex` varchar(255) NOT NULL COMMENT 'Exchange sold to',
+  `buy_symbol` varchar(255) NOT NULL COMMENT 'Buy symbol',
+  `sell_symbol` varchar(255) NOT NULL COMMENT 'Sell symbol',
+  `buy_bid` float(16,8) NOT NULL COMMENT 'Buy bid value',
+  `sell_ask` float(16,8) NOT NULL COMMENT 'Sell ask value',
+  `buy_quote` float(16,2) NOT NULL COMMENT 'Buy value (Quote value)',
+  `sell_base` float(16,8) NOT NULL COMMENT 'Sell value (Base value)',
   `profit` float(10,2) NOT NULL COMMENT 'Potential profit (USD)',
-  `exchanges` mediumtext NOT NULL COMMENT 'Buy and sell exchange information',
   `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Added date',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
